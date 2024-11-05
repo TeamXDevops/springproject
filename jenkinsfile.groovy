2451084d84e43611b4a769b2ea2dc23c65f068a3
mohamedzrig27 <mohamedzrig17@outlook.Com>
2024-11-05 16:33:59 +0200

    Push final

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -29,7 +29,7 @@ pipeline {
             }
         }
 
-        stage('SonarQube Analysis') {
+        /*stage('SonarQube Analysis') {
             steps {
                 script {
                     withCredentials([usernamePassword(credentialsId: 'Sonarqube-Credential', usernameVariable: 'SONAR_USER', passwordVariable: 'SONAR_PASSWORD')]) {
@@ -43,7 +43,7 @@ pipeline {
                     }
                 }
             }
-        }
+        }*/
         
         stage('Maven Deploy') {
             steps {
