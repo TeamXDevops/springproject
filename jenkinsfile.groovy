19a2dbcf7fe012eae7ff38ee008357c3bd97d45e
mohamedzrig27 <mohamedzrig17@outlook.Com>
2024-11-13 11:55:54 +0200

    Update Mail notification stage

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -62,19 +62,6 @@ pipeline {
             }
         }
 
-        stage('Mail notification') {
-            steps {
-                script {
-                    emailext(
-                            subject: "Build Successful: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
-                            body: """<p>Good news!</p>
-                    <p>The build <strong>${env.JOB_NAME} #${env.BUILD_NUMBER}</strong> completed successfully.</p>
-                    <p>Check it out at: <a href="${env.BUILD_URL}">${env.BUILD_URL}</a></p>""",
-                            to: 'mohamed.zrig@esprit.tn'
-                    )
-                }
-            }
-        }
 
 
         stage('Build Docker Image') {
